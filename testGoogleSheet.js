f75dd96a2f11c905726e82ff5cfe91be99f2cfc5
luffyheng <luffyheng@gmail.com>
2025-05-13 00:09:37 +0800

    Delete testGoogleSheet.js

--- testGoogleSheet.js
+++
@@ -1,9 +0,0 @@
-// backend/testGoogleSheet.js
-
-const { getCourseLink } = require('./googleSheet');
-
-(async () => {
-  const sku = 'a281'; // Replace with a SKU from your sheet
-  const result = await getCourseLink(sku);
-  console.log(result);
-})();
